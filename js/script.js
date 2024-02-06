b29cd275d03f53efd5fb296f06bdae44e7303260
Leonardo Kotcheski Filipiaki <duckgames875@gmail.com>
2024-02-06 09:33:50 -0300

    Day Two

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const perguntas = [
     correta: 1,
   },
   {
-    pergunta: "Como declarar uma variável em JavaScript?",
+    pergunta: "Como declarar uma variável constante em JavaScript?",
     respostas: [
       "var nomeDaVariavel = valor;",
       "let nomeDaVariavel = valor;",
@@ -24,7 +24,7 @@ const perguntas = [
       "Uma função que realiza uma tarefa específica.",
       "Um valor básico como string, number ou boolean.",
     ],
-    correta: 3,
+    correta: 2,
   },
   {
     pergunta: "Qual a diferença entre '=='' e '===' em JavaScript?",
@@ -91,25 +91,41 @@ const perguntas = [
   },
 ];
 
-const quiz = document.querySelector('#quiz')
+const quiz = document.querySelector('#quiz');
 
 const template = document.querySelector('template');
 
+const corretas = new Set();
 
+const totalDePerguntas = perguntas.length;
+
+const mostrarTotal = document.querySelector('#acertos span');
+
+mostrarTotal.textContent = `${corretas.size} de ${totalDePerguntas}`
 // Loop/Laço de repetição
 for (const item of perguntas) {
     const quizItem = template.content.cloneNode(true);
 
-    quizItem.querySelector('h3').textContent = item.pergunta
+    quizItem.querySelector('h3').textContent = item.pergunta;
     
     for (let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true);
 
-        dt.querySelector('span').textContent = resposta
+        dt.querySelector('span').textContent = resposta;
+        dt.querySelector('input').setAttribute('name', `pergunta-${perguntas.indexOf(item)}`);
+        dt.querySelector('input').value = item.respostas.indexOf(resposta);
+        dt.querySelector('input').onchange = (event) => {
+          const estaCorreta = event.target.value == item.correta;
+          corretas.delete(item);
+          if(estaCorreta) {
+            corretas.add(item);
+          }
+          mostrarTotal.textContent = `${corretas.size} de ${totalDePerguntas}`;
+        }
 
-        quizItem.querySelector('dl').appendChild(dt)
+        quizItem.querySelector('dl').appendChild(dt);
     }
 
-    quizItem.querySelector('dl dt').remove()
+    quizItem.querySelector('dl dt').remove();
     quiz.appendChild(quizItem);
 }
